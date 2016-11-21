6b72bc72b5960c886c6fb6349c6c4cd7a3291d43
Eric Coleman <ecoleman@pillartechnology.com>
2016-11-21 13:39:22 -0500

    install vim plug before symlinking dotfiles

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+dotfiles_repo_dir=${HOME}/.homesick/repos/dotfiles
+
 echo "gem: --user-install" > $HOME/.gemrc
+
+$dotfiles_repo_dir/install_vim_plug.sh
 
 gem install homesick
 
 homesick clone epiccoleman/dotfiles
 homesick symlink dotfiles --force
-
-dotfiles_repo_dir=${HOME}/.homesick/repos/dotfiles
 
 $dotfiles_repo_dir/install_pure_prompt.sh
