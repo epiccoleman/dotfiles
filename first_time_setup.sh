b76d0e2979aa019e1eb9a8637c4dfb1fed2d49e7
Eric Coleman <ecoleman@pillartechnology.com>
2016-11-20 22:42:12 -0500

    Force overwrite of existing dotfiles when running first_time_setup

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -5,7 +5,7 @@ echo "gem: --user-install" > $HOME/.gemrc
 gem install homesick
 
 homesick clone epiccoleman/dotfiles
-homesick symlink dotfiles
+homesick symlink dotfiles --force
 
 dotfiles_repo_dir=${HOME}/.homesick/repos/dotfiles
 
