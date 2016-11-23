dbe1714cc4758bebd532e4865ad408637ffceab7
Eric Coleman <ecoleman@pillartechnology.com>
2016-11-22 22:50:35 -0500

    Move scripts to scripts dir and refactor install script to reflect
    Also move vim plug call to the end of the script, since it's
    interactive. Don't know why I thought that was a good idea

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 dotfiles_repo_dir=${HOME}/.homesick/repos/dotfiles
+script_dir=${dotfiles_repo_dir}
 
+# sets up ruby gems to install to home dir 
+# (no need for sudo on `gem install`)
 echo "gem: --user-install" > $HOME/.gemrc
-
-$dotfiles_repo_dir/install_vim_plug.sh
 
 gem install homesick
 
 homesick clone epiccoleman/dotfiles
 homesick symlink dotfiles --force
 
-$dotfiles_repo_dir/install_pure_prompt.sh
+${script_dir}/install_pure_prompt.sh
+${script_dir}/install_vim_plug.sh
