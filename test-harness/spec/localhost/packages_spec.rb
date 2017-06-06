a426a20d7c9bcdc89cd532cebc3bc2074afa988c
Eric Coleman <eric@epiccoleman.com>
2017-06-06 13:46:09 -0400

    add some tests around package installation

--- test-harness/spec/localhost/packages_spec.rb
+++ test-harness/spec/localhost/packages_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe package('docker'), :if => os[:family] == 'darwin' do
+#describe package('iterm2'), :if => os[:family] == 'darwin' do
+#  it { should be_installed }
+#end
+
+describe package('tree') do
+  it { should be_installed }
+end
+
+describe package('tmux') do
+  it { should be_installed }
+end
+
+describe package('wget') do
+  it { should be_installed }
+end
+
+describe package('the_silver_searcher'), :if => os[:family] == 'darwin' do
   it { should be_installed }
 end
