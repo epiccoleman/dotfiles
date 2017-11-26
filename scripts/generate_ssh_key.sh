0e045369d2fabe2bb394147370458aa45be379b3
Eric Coleman <eric@epiccoleman.com>
2017-11-25 23:34:18 -0500

    add user error handling to generate_ssh_key

--- scripts/generate_ssh_key.sh
+++ scripts/generate_ssh_key.sh
@@ -4,7 +4,12 @@ echo "############################################################"
 echo "#                   generate_ssh_key.sh                    #"
 echo "############################################################"
 
-echo "[generate_ssh_key] Enter comment for this key (don't fuck it up): " 
+echo "[generate_ssh_key] Enter comment for this key (don't fuck it up): "
 read COMMENT
+
+echo "[generate_ssh_key] You usually fuck this up, second chance? (hit enter to skip): "
+read YOU_FUCKED_IT_UP
+
+[ ! -z $YOU_FUCKED_IT_UP ] && COMMENT=$YOU_FUCKED_IT_UP
 
 ssh-keygen -t rsa -b 4096 -C "${COMMENT}"
