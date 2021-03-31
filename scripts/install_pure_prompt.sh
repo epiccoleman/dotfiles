a574915680e110d009285be55fd82960c9dfd802
Eric Coleman <eric@epiccoleman.com>
2021-03-31 17:04:39 -0400

    deprecate install_pure_prompt script

--- scripts/install_pure_prompt.sh
+++ scripts/install_pure_prompt.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# this is the dumbest way of getting the path but it's fine if all you want to do is make 
-# sure that execution happens in the dir where the script lives and you won't be using 
+# this is the dumbest way of getting the path but it's fine if all you want to do is make
+# sure that execution happens in the dir where the script lives and you won't be using
 # symlinks
 CASTLE_PATH="$(dirname $0)/.."
 
-cd $CASTLE_PATH
+# cd $CASTLE_PATH
 
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
 
-pushd pure-zsh-prompt
-git pull origin master
-git checkout master
-popd 
+# pushd pure-zsh-prompt
+# git pull origin master
+# git checkout master
+# popd
 
-mkdir -p $HOME/.zfunctions
-ln -sf $PWD/pure-zsh-prompt/async.zsh $HOME/.zfunctions/async
-ln -sf $PWD/pure-zsh-prompt/pure.zsh $HOME/.zfunctions/prompt_pure_setup
+# mkdir -p $HOME/.zfunctions
+# ln -sf $PWD/pure-zsh-prompt/async.zsh $HOME/.zfunctions/async
+# ln -sf $PWD/pure-zsh-prompt/pure.zsh $HOME/.zfunctions/prompt_pure_setup
+
+echo "This no longer works. Go follow the instructions in the pure readme. Or write a better script. Hello from your past self!"
